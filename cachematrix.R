184493c34c8e3577204b1cc3f6f3c9e38ea4dbd6
bgmadigan <bgmadigan@hotmail.com>
2014-07-26 12:01:54 +1000

    Final updates before assignmnet submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
 ##
+##
 ## R Programmimg - Assignment 2
 ## ----------------------------
 ## 
@@ -11,12 +12,14 @@
 ##                  If the inverse has already been calculated (and the matrix has not changed), 
 ##                  then the cachesolve should retrieve the inverse from the cache.
 ##
-
-
+##
 makeCacheMatrix <- function(x = matrix()) {
 ##
 ##   makeCacheMatrix takes as its input:
-##      'x' - square matrix
+##
+##      'x' - a square matrix 
+##        
+##   NOTE: no checks are performed to ensure that x is a square matrix
 ##
 ##   1.set the value of the matrix
 ##   2.get the value of the matrix
@@ -40,14 +43,17 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
 ##
 ##   cacheSolve takes as its input:
+##
 ##      'x' - square matrix
+##        
+##   NOTE: no checks are performed to ensure that x is a square matrix
 ##
 ##   If the inverse matrix has already been determined
 ##      return the cached matrix (to save calculation time)
 ##   otherwise
 ##      use "solve" todetermine the inverse matrix
 ##      return the matrix
-       
+##       
        m <- x$getinverse()
         if(!is.null(m)) {
                 message("getting cached matrix")
